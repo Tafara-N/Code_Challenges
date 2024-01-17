c70c8f4b54a2e4b0e46c18d539e34c86bf6fa923
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-01-17 11:16:25 +0200

    A function that adds binary numbers and returns a string representation

---
+++ CodeWars/03-BinaryAddition/add_binary.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+A function that adds binary numbers and returns a string representation
+"""
+
+def add_binary(a,b):
+    decimal_sum = a + b
+    
+    # Converting the decimal sum to binary and removing '0b' prefix
+    binary_sum = bin(decimal_sum)[2:]
+    
+    return binary_sum
+
+result = add_binary(1, 1)
+print(result)

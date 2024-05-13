f237725b03e8d1a5bee4f21ce77baf823cc279e2
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-13 22:43:29 +0200

    Print a string in a specific format.

---
+++ W3-Resource/Python/twinkle/twinkle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Print a string in a specific format
+
+print("""Twinkle, twinkle, little star,
+	How I wonder what you are!
+		Up above the world so high,
+		Like a diamond in the sky.
+Twinkle, twinkle, little star,
+	How I wonder what you are""")

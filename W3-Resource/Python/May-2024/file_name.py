aec74832acaa764da64c8046428fba8577e181ad
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-14 02:53:51 +0200

    Deleted file_name.py

--- W3-Resource/Python/May-2024/file_name.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-# Program accepts a filename from the user and prints the extension of the file
-
-filename = input("Enter the filename: ")
-
-# Split the filename by "." and get the last element of the list
-file_extension = filename.split(".")[-1]
-print(f"The extension of the file is: {"." + file_extension}")

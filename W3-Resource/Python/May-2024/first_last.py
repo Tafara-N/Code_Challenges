aa6997ffab5a1052ccbcce0016ed64b8242d4a0d
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-14 02:58:25 +0200

    Program to display the first and last colors from a list.

---
+++ W3-Resource/Python/May-2024/first_last.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Program to display the first and last colors from the following list
+
+color_list = ["Red","Green","White" ,"Black"]
+
+print(f"The first color in the list is: {color_list[0]}\n\
+The last color in the list is: {color_list[-1]}")

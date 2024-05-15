4a9918f2b9640f3c34ae4d83d36a90e4fb6a5778
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-15 14:03:06 +0200

    Program to display the examination schedule.

---
+++ W3-Resource/Python/May-2024/exam_date.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Program to display the examination schedule
+
+exam_st_date = (11, 12, 2014)
+date = exam_st_date
+
+print("The examination will start from : {} / {} / {}".format(date[0],
+                                                              date[1],
+                                                              date[2]))

1bca4697c9cb05468198389e603d4a54738a4da0
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-18 01:01:40 +0200

    Prints the calendar for a given month and year.

---
+++ W3-Resource/Python/May-2024/calendar_month.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from calendar import month
+
+
+def main():
+    """
+    Main function to get the year and month from the user.
+    """
+
+    year = int(input("Enter the year: "))
+    mm = int(input("Enter the month: "))
+
+    print_calendar(year, mm)
+
+
+def print_calendar(yyyy, mm):
+    """
+    Prints the calendar for a given month and year.
+    :param yyyy: Year
+    :param mm: Month
+    """
+    print("\n", month(yyyy, mm))
+
+
+if __name__ == "__main__":
+    main()

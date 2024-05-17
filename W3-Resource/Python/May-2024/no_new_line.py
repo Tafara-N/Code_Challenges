bfc7808f2f1091fe33cfe0e15feb8031f292770d
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-18 01:42:48 +0200

    Program prints the following 'here document'.

---
+++ W3-Resource/Python/May-2024/no_new_line.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Program prints the following 'here document'
+
+string = """a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example"""
+
+print(string)

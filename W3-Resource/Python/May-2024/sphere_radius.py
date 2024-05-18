c33ec245ba38e6f784fd8ab26040fe391d8bd134
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-18 13:45:56 +0200

    Find the radius of the sphere whose volume is given.

---
+++ W3-Resource/Python/May-2024/sphere_radius.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+A sphere has a volume equal to 356.82 cu.cm. Find the radius of the sphere
+
+Formula: radius = (3 * volume / (4 * π)) ^ (1 / 3)
+"""
+
+from math import pi
+
+volume = 356.82
+
+radius = (3 * volume / (4 * pi)) ** (1 / 3)
+
+print(f"The radius of a sphere with volume {volume} is {radius:.2f}")

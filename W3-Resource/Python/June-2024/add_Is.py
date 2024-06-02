740823f68fd6eb351be3e5709e6358c025fd0943
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-06-03 01:47:38 +0200

    Program to get a newly-generated string from a given string where 'Is' has been added to the front

---
+++ W3-Resource/Python/June-2024/add_Is.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Program to get a newly-generated string from a given string where "Is" has been
+added to the front
+Return the string unchanged if the given string already begins with "Is"
+"""
+
+def add_Is(string):
+    """
+    Function to add "Is" to the front of a given string
+    """
+    if string[:2] == "Is":
+        return f"Already prefixed with 'Is': {string}"
+    return f"Is {string}"
+
+if __name__ == "__main__":
+    string = input("Enter a string: ")
+    print(add_Is(string))

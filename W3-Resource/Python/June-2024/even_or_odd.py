27d4d14fb1cb62be71263b3eb60718303bd2d731
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-06-03 02:32:26 +0200

    Program that determines whether a given number (accepted from the user) is even or odd, and prints an appropriate message to the user.

---
+++ W3-Resource/Python/June-2024/even_or_odd.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Program that determines whether a given number (accepted from the user) is even
+or odd, and prints an appropriate message to the user
+"""
+
+
+def even_or_odd(number):
+    """
+    Checks whether a number is even or odd
+    """
+
+    if number % 2 == 0:
+        return f"{number} is even."
+    return f"{number} is odd."

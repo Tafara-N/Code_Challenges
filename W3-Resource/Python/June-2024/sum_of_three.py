8d9337b305d33f0c86d601cd5f4225ffd5ed0c38
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-06-03 02:30:58 +0200

    Program to calculate the sum of three given numbers.

--- W3-Resource/Python/June-2024/sum_of_three.py
+++ W3-Resource/Python/June-2024/sum_of_three.py
@@ -5,6 +5,7 @@ Program to calculate the sum of three given numbers
     If the values are equal, return three times their sum
 """
 
+
 def sum_of_three(first, second, third):
     """
     Function to calculate the sum of three given numbers
@@ -13,9 +14,10 @@ def sum_of_three(first, second, third):
         return 3 * (first + second + third)
     return first + second + third
 
+
 if __name__ == "__main__":
     first = int(input("Enter the first number: "))
     second = int(input("Enter the second number: "))
     third = int(input("Enter the third number: "))
-    
+
     print("Sum of three numbers is: ", sum_of_three(first, second, third))

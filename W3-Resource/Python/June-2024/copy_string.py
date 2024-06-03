03e3595033f162b15080f7b9e60123785cc28f0d
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-06-03 02:30:33 +0200

    Program that returns a string that is n (non-negative integer) copies of a given string

--- W3-Resource/Python/June-2024/copy_string.py
+++ W3-Resource/Python/June-2024/copy_string.py
@@ -5,11 +5,13 @@ Program that returns a string that is n (non-negative integer) copies of a
 given string
 """
 
+
 def copy_string(string, number):
     """
     Function to return a string that is n copies of a given string
     """
     return string * number
+
 
 if __name__ == "__main__":
     string = input("Enter a string: ")

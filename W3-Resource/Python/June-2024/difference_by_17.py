54cec7d47d9d934e0ebc171e5ef73833a060807a
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-06-03 02:30:12 +0200

    Program to calculate the difference between a given number and 17.

--- W3-Resource/Python/June-2024/difference_by_17.py
+++ W3-Resource/Python/June-2024/difference_by_17.py
@@ -5,11 +5,13 @@ Program to calculate the difference between a given number and 17
     If the number is greater than 17, return twice the absolute difference
 """
 
+
 def difference_by_17(number):
     if number <= 17:
         return 17 - number
     else:
         return 2 * (number - 17)
+
 
 if __name__ == "__main__":
     number = int(input("Enter a number: "))

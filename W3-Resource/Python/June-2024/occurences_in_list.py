efdbddbbf67886a97c90facebcdfab4b0808e366
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-06-03 11:09:31 +0200

    Program to count the number 4 in a given list.

--- W3-Resource/Python/June-2024/occurences_in_list.py
+++ W3-Resource/Python/June-2024/occurences_in_list.py
@@ -11,5 +11,19 @@ def occurences_in_list(number):
     return f"Number occurs {numbers.count(number)} times."
 
 
+# OR THIS WAY
+
+
+def occurences_in_list2(numbers):
+    count = 0
+
+    for number in numbers:
+        if number == 4:
+            count += 1
+    return f"Number occurs {count} times."
+
+print(occurences_in_list2([4, 20, 1, 300, 4.4, 4, 35, 20, 4]))
+
+
 if __name__ == '__main__':
     print(occurences_in_list(4))

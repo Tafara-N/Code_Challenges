470215844832e8f61bb8432099bf2e4328352519
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-06-07 22:39:48 +0200

    Program to create a histogram from a given list of integers.

--- W3-Resource/Python/June-2024/list_histogram.py
+++ W3-Resource/Python/June-2024/list_histogram.py
@@ -5,10 +5,12 @@ Program to create a histogram from a given list of integers
 """
 
 def list_histogram(values):
+    histogram = ""
     for value in values:
-        print('#' * value)
+        histogram += f"{'#' * value}\n"
+    return histogram.rstrip()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     values = [1, 5, 8, 3, 1, 5, 8, 3, 1, 5, 8, 3]
     print(list_histogram(values))

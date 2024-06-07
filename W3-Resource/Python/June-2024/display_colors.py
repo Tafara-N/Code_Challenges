a25b8cf9181090a7c7cc00f6b72103ece10ec8ab
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-06-07 23:14:06 +0200

    Program that prints out all colors from color_list_1 that are not present in color_list_2.

---
+++ W3-Resource/Python/June-2024/display_colors.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+Program that prints out all colors from color_list_1 that are not present in
+color_list_2
+"""
+
+def display_colors(color_list_1, color_list_2):
+    """
+    Function that prints out all colors from color_list_1 that are not present
+    in color_list_2.
+    """
+    for color in color_list_1:
+        if color not in color_list_2:
+            print(color)
+
+
+if __name__ == "__main__":
+    color_list_1 = set(["White", "Black", "Red"])
+    color_list_2 = set(["Red", "Green"])
+
+    display_colors(color_list_1, color_list_2)
+    print("\nDone...")

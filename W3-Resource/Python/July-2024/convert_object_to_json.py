11367bb8fcceadf739070afb0f63e51ffce9fad1
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-27 14:36:27 +0200

    Program to convert Python object to JSON data.

--- W3-Resource/Python/July-2024/convert_object_to_json.py
+++ W3-Resource/Python/July-2024/convert_object_to_json.py
@@ -4,3 +4,27 @@
 Program to convert Python object to JSON data
 """
 
+import json
+
+
+def obj_to_json(python_object: dict) -> str:
+    """
+    Function converts Python object to JSON data
+
+    Paramaters
+        python_object: Python object to be converted
+
+    Return
+        JSON data: str
+    """
+
+    try:
+        json_data = json.dumps(python_object)
+        return json_data
+    except TypeError:
+        return "Invalid Python object"
+
+
+if __name__ == "__main__":
+    python_object = {"a": 1, "b": 2, "c": 3}
+    print(obj_to_json(python_object))

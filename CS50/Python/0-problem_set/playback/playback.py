3a2abc3bc475e3380005d413fc852a50fff032fa
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-10-15 16:41:06 +0200

    Prompts user for input and then outputs that same input, replacing each space with `...`.

---
+++ CS50/Python/0-problem_set/playback/playback.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Program prompts the user for input and then outputs that same input, replacing
+each space with `...` (i.e., three periods).
+
+"""
+
+prompt = input(">>>").replace(" ", "...")
+
+print(prompt)

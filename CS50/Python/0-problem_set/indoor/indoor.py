0acd2f5e7342e82889ab635a43108621bc66c01a
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-10-15 16:23:25 +0200

    Print user input in lowercase.

---
+++ CS50/Python/0-problem_set/indoor/indoor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+WRITING IN ALL CAPS IS LIKE YELLING.
+
+Best to use your “indoor voice” sometimes, writing entirely in lowercase.
+
+"""
+
+prompt = input(">>> ").lower()
+
+print(prompt)
